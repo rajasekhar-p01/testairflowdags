f32bb52d1a03f457a9264ac11dc4e3297f5815fa
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-04-30 15:53:56 +0530

    sample DAF file

---
+++ my_simple_dag5_5.py
@@ -0,0 +1,43 @@
+import datetime as dt
+import time
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+def greet():
+    print('Write in file')
+    print('Sleep 90 seconds')
+    time.sleep(90)
+    return 'Greeted'
+
+
+def respond():
+    return 'Greet Responded Again'
+
+
+default_args = {
+    'owner': 'Raja',
+    'start_date': dt.datetime(2018, 9, 24, 10, 00, 00),
+    'concurrency': 1,
+    'retries': 0
+}
+
+with DAG('my_simple_dag5_5',
+         catchup=False,
+         default_args=default_args,
+         schedule_interval='*/10 * * * *',
+         # schedule_interval=None,
+         ) as dag:
+    opr_hello = BashOperator(task_id='say_Hi5_5',
+                             bash_command='echo "Hi!!"')
+
+    opr_greet = PythonOperator(task_id='greet5_5',
+                               python_callable=greet)
+    opr_sleep = BashOperator(task_id='sleep_me5_5',
+                             bash_command='sleep 5')
+
+    opr_respond = PythonOperator(task_id='respond5_5',
+                                 python_callable=respond)
+
+opr_hello >> opr_greet >> opr_sleep >> opr_respond
