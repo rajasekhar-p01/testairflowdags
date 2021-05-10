6134dbcbbc5a758a05a3b1197b8e1a5277802898
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-10 21:01:14 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -24,7 +24,7 @@ dag = DAG(
 )
 
 # Generate 4 tasks
-tasks = ["task{}".format(i) for i in range(150, 200)]
+tasks = ["task{}".format(i) for i in range(50, 80)]
 example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
 
 org_dags = []
