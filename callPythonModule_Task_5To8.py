dd9c634616479135eeef256289c69b113809707a
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-11 13:39:52 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -39,7 +39,7 @@ def pull_secret_value():
     return retrieved_secret.value
 
 # Generate 4 tasks
-tasks = ["task{}".format(i) for i in range(50, 60)]
+tasks = ["task{}".format(i) for i in range(50, 55)]
 example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
 python_pull_secret = PythonOperator(task_id="python_pull_secret", python_callable=pull_secret_value)
 
