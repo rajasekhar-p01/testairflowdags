5ddaf5730389ea805c1c42f567a9a6f7642fce69
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-11 13:43:38 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -53,7 +53,7 @@ for python_task in tasks:
         image="testcontainerkubernetraja.azurecr.io/argspython",
         image_pull_secrets='testcontainerkubernetraja',
         cmds=["python","name.py"],
-        arguments=[{secret_value_op},"Second","Third"],
+        arguments=["First","Second","Third"],
         labels={"foo": "bar"},
         image_pull_policy="Always",
         name=python_task,
