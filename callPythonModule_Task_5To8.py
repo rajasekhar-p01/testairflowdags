39814d8381298c1ce88c3b7d3100cc527c3a821a
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-11 20:53:46 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -29,8 +29,7 @@ dag = DAG(
     start_date=days_ago(0)
 )
 
-def pull_secret_value(**kwargs):
-    ti = kwargs['ti']
+def pull_secret_value():
     KVUri = f"https://airflow-keyvault-3.vault.azure.net"
     credential = ClientSecretCredential('d3c91205-02f7-4bba-bd33-0fde50b3a8b4', '0a34166b-75ea-45c5-89a1-b2f62b9ca602', 'e~scpuZ5R-65ueaItpAReX0T-6~kV-j~HU')
     client = SecretClient(vault_url=KVUri, credential=credential)
@@ -38,7 +37,6 @@ def pull_secret_value(**kwargs):
     retrieved_secret = client.get_secret(secretName)
     print(f"Your secret is '{retrieved_secret.value}'.")
     secret_value_op = retrieved_secret.value
-    ti.xcom_push(key='secretname3', value = 'sspp')
     #Variable.set(retrieved_secret.value, secret_value_op)
     #return retrieved_secret.value
     return { 'subject': 'secret'}
