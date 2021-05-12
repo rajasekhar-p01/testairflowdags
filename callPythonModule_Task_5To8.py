06174d5ca57aed6d18697d0b87cbbc7c3e2e6d2a
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-12 10:00:13 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -28,7 +28,7 @@ dag = DAG(
     start_date=days_ago(0)
 )
 
-def pull_secret_value(ds, **kwargs):
+def pull_secret_value():
     KVUri = f"https://airflow-keyvault-3.vault.azure.net"
     credential = ClientSecretCredential('d3c91205-02f7-4bba-bd33-0fde50b3a8b4', '0a34166b-75ea-45c5-89a1-b2f62b9ca602', 'e~scpuZ5R-65ueaItpAReX0T-6~kV-j~HU')
     client = SecretClient(vault_url=KVUri, credential=credential)
@@ -39,9 +39,9 @@ def pull_secret_value(ds, **kwargs):
     return retrieved_secret.value
 
 # Generate 4 tasks
-tasks = ["task{}".format(i) for i in range(50, 52)]
+tasks = ["task{}".format(i) for i in range(50, 60)]
 example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
-python_pull_secret = PythonOperator(task_id="python_pull_secret", provide_context=True, python_callable=pull_secret_value)
+python_pull_secret = PythonOperator(task_id="python_pull_secret", python_callable=pull_secret_value)
 
 
 org_dags = []
