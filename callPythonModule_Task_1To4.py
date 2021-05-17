c425d015ed1934157715dfbc1049df6d5a24502b
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-17 15:44:45 +0530

    Update callPythonModule_Task_1To4.py

--- callPythonModule_Task_1To4.py
+++ callPythonModule_Task_1To4.py
@@ -36,7 +36,7 @@ for python_task in tasks:
 
     org_node = KubernetesPodOperator(
         namespace='kube-node-lease',
-        image="testcontainerforarfl.azurecr.io/argspython",
+        image="airflowacrdemocontainer.azurecr.io/argspython",
         image_pull_secrets='acrsecret',
         cmds=["python","name.py"],
         arguments=["Pudota","Raja","Sekhar"],
