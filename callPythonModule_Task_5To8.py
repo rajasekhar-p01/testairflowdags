163c08095bcc3bcb7269934465c1b06fa99421ec
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-20 19:09:44 +0530

    Update callPythonModule_Task_5To8.py

--- callPythonModule_Task_5To8.py
+++ callPythonModule_Task_5To8.py
@@ -19,7 +19,7 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
+    'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
