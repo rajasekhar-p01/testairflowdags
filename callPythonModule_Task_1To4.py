35e971a4b90d1eac66f553c50316e8273ab7191b
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-20 15:52:17 +0530

    Update callPythonModule_Task_1To4.py

--- callPythonModule_Task_1To4.py
+++ callPythonModule_Task_1To4.py
@@ -18,6 +18,7 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
+resource1={"request_memory":"40Mi","request_cpu":"10m","limit_memory":"40Mi","limit_cpu":"10m"}
 
 dag = DAG(
     'callPythonModule_Task_1To4',
@@ -42,6 +43,7 @@ for python_task in tasks:
         arguments=["Pudota","Raja","Sekhar"],
         labels={"foo": "bar"},
         image_pull_policy="Always",
+        resources=resource1,
         name=python_task,
         task_id=python_task,
         is_delete_operator_pod=False,
