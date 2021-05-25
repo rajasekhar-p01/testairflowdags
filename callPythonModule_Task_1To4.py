11a60935f604aac5dba91706deec0e7032f66be0
rajasekhar-p01 <71096241+rajasekhar-p01@users.noreply.github.com>
2021-05-25 18:40:03 +0530

    Update callPythonModule_Task_1To4.py

--- callPythonModule_Task_1To4.py
+++ callPythonModule_Task_1To4.py
@@ -15,8 +15,8 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=8),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
 }
 #resource1={"request_memory":"1Mi","request_cpu":"1m","limit_memory":"1Mi","limit_cpu":"1m"}
 
